66da74512071ec5568f3b44b61997f965bfb9075
Faidlur Rohman <faidrohman@outlook.com>
2023-07-31 21:03:05 +0700

    init cors lib

---
+++ application/libraries/Httpcors.php
@@ -0,0 +1,23 @@
+<?php
+defined("BASEPATH") OR exit("No direct script access allowed");
+ 
+class Httpcors {
+    
+    protected $CI;
+	
+	public function __construct()
+	{
+		$this->CI = &get_instance();
+    }
+    
+    function _authenticate_CORS()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: ACCEPT, ORIGIN, X-REQUESTED-WITH, CONTENT-TYPE, AUTHORIZATION, Client-ID, Secret-Key, Authorization, User-ID');
+        if ("OPTIONS" === $_SERVER['REQUEST_METHOD']) {
+            die();
+        }
+    }
+}
