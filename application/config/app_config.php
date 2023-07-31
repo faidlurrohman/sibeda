f0aa6103d15ae38a92d06d66f56484b353f68f07
Faidlur Rohman <faidrohman@outlook.com>
2023-07-31 21:02:48 +0700

    rename config

--- application/config/app_config.php
+++ application/config/app_config.php
@@ -1,5 +1,5 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['APP_NAME'] = 'SILARA KAB KOTA';
+$config['APP_NAME'] = 'SIBEDA PROVINSI KEPRI';
 $config['KEY_PASS'] = 'f4bd67bfca1e2ff1ff1b1a28979bbbe5_@_';
 
