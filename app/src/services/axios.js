faf3072cf47599fdfc142a6306f701708ad69103
Faidlur Rohman <faidrohman@outlook.com>
2023-08-06 03:02:27 +0700

    refactor handle error

--- app/src/services/axios.js
+++ app/src/services/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { store } from "../store";
 import { logoutAction } from "../store/actions/session";
-import { ping } from "./ping";
 import { swal } from "../helpers/swal";
 
 const axiosInstance = axios.create({
@@ -28,45 +27,24 @@ axiosInstance.interceptors.request.use((config) => {
 
 axiosInstance.interceptors.response.use(
 	(response) => {
-		return response;
+		// console.log("RESPONSE :::", response?.data);
+		return response?.data;
 	},
 	async (error) => {
+		// console.log("ERROR :::", error);
 		// due internet connection
 		if (!navigator.onLine) {
 			swal("Tidak ada koneksi internet", "warning");
 		} else {
-			// check connection db
-			ping().then((p) => {
-				if (p?.data?.status) {
-					// kalau cookies expire langsung tendang ke login
-					if (error?.response?.status === 401) {
-						let msg = error?.response?.data?.message;
-
-						if (msg) {
-							swal(`${msg}.`, "warning");
-						} else {
-							swal("Sesi anda berakhir, silahkan masuk kembali.", "warning");
-							store.dispatch(logoutAction());
-						}
-					} else {
-						// any error was handled
-						if (
-							error?.code === "ERR_BAD_RESPONSE" &&
-							error?.response?.data !== ""
-						) {
-							swal(`${error?.response?.data?.message || error.message}.`);
-						} else {
-							// any error here or not handle
-							swal("Opps!!!");
-							console.log("error?.code", error?.code);
-						}
-					}
-				} else {
-					// due db connection
-					swal("Tidak ada koneksi database");
-					return error;
-				}
-			});
+			if (
+				error?.response?.status === 401 ||
+				error?.response?.data?.code === 401
+			) {
+				swal(`${error?.response?.data?.message || error.message}.`, "warning");
+				store.dispatch(logoutAction());
+			} else {
+				swal(`${error?.response?.data?.message || error.message}.`);
+			}
 		}
 
 		return error;
