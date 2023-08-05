ab598f75d951cc344665f15bb210f3341bec18c9
Faidlur Rohman <faidrohman@outlook.com>
2023-08-06 02:22:56 +0700

    refactor function and response

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -2,7 +2,7 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
+    
 function get_token() 
 {
     $CI = &get_instance();
@@ -16,11 +16,12 @@ function model_response($query, $type = 0)
 {
     $CI = &get_instance();
     $e = $CI->db->error();
+    $response = res_type($type, $query);
 
-    if ($e['code'] == '00000') {
-        return res_type($type, $query);
+    if ($response['code'] == 200 && $e['code'] == '00000') {
+        return $response;
     } else {
-        return err_db($e);
+        return err_msg($response);
     }
 }
 
@@ -30,97 +31,52 @@ function res_type($type, $query)
     $arr = $query->result_array();
 
     switch ($type) {
-        // get data     
+        // read data -> read_data procedure
         case 0:
-            $data['code'] = isset($row['__code']) ? $row['__code'] : 0; 
-            $data['data'] = isset($row['__code']) ? array() : ($row['__code'] > 0 ? array() : $arr);
-            $data['message'] = err_msg($query);
+            $data = json_decode($row['@read'], true);
+            $data['data'] = $data['total'] == 0 ? [] : $data['data'];
             break;
-        // get list
+
+        // list data -> list_data procedure
         case 1:
-            $data['data'] = isset($row['__code']) ? array() : ($row['__res_count'] == 0 ? array() : $arr);
-            $data['code'] = isset($row['__code']) ? $row['__code'] : 0;
-            $data['message'] = err_msg($query);
+            $data = json_decode($row['@list'], true);
+            $data['data'] = $data['total'] == 0 ? [] : $data['data'];
             break;
-        // save / update / delete
+
+        // (save || update || delete) data -> cud_data procedure 
         case 2:
-            $data['id'] = $row['__new_id'];
-            $data['code'] = $row['__code'] ? $row['__code'] : 0;
-            $data['message'] = err_msg($query);
+            $data = json_decode($row['@cud'], true);
             break;
-        // delete
-        case 3:
-            $data['code'] = 0;
-            $data['data'] = $row;
-            break;
-        // single row
-        case 4:
-            $data = $row;
-            break;
-        // auth
+
+        // auth -> login function
         case 10:
-            $data['data'] = $row;
-            $data['code'] = $row['token'] ? 0 : ($row['__res_data'] == 1 ? 0 : 1 );
-            $data['message'] = $row['token'] ? '' : ($row['__res_data'] == 1 ? '' : 'Nama pengguna atau kata sandi salah' );
+            $data = json_decode($row['login'], true);
+            break;
+
+        // validating token -> validate_token function
+        case 11:
+            $data = json_decode($row['validate_token'], true);
             break;
     }
 
     return $data;
 }
 
-function err_msg($query, $message = ''){
-    $code = $query->row_array()['__code'];
-
-    switch ($code) {
-        // data tidak ada
-        case 101:
-            $message = 'Data tidak ditemukan';
-            break;
-        // data duplicate
-        case 102:
-            $message = 'Data sudah ada';
-            break;
-        // mode salah atau di luar salah satu C/U/D
-        case 103:
-            $message = 'Data yang dikirim tidak sesuai';
-            break;
-        // schema tidak ada
-        case 104:
-            $message = 'Data yang dikirim tidak sesuai';
-            break;
-        // user tidak ada
-        case 105:
-            $message = 'Pengguna tidak terdaftar untuk melakukan perubahan data';
-            break;
-        // format parameter salah
-        case 106:
-            $message = 'Data yang dikirim tidak sesuai';
-            break;
-        // data alokasi kota tidak bisa dirubah
-        case 110:
-            $message = 'Alokasi tidak bisa diubah, karena sudah terdapat transaksi';
-            break;
-    }
-
-    return $message;
-}
-
-function err_db($error){
+// MYSQL ERROR REFERENCE => https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html
+function err_msg($error){
     $code = $error['code'];
 
     switch ($code) {
-        // POSTGRES ERROR CODE https://www.postgresql.org/docs/current/errcodes-appendix.html
         // data duplicate
-        case str_contains($code, '23505/7'):
-            $code = 102;
+        case 1062:
             $message = 'Data sudah ada';
             break;
 
         default : 
-            $message = 'Error Postgres Code : '. $code;
+            $message = $error['message'];
     }
 
-    return ['code' =>  $code, 'message' => $message];
+    return ['code' =>  $code, 'message' => $message, 'data' => [], 'query' => $error['query']];
 }
 
 function set_order($value){
