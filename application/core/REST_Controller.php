e30744f1deae93ae6c507c9aba3284082d97dfc7
Faidlur Rohman <faidrohman@outlook.com>
2023-08-29 02:45:05 +0700

    make self rest

---
+++ application/core/REST_Controller.php
@@ -0,0 +1,61 @@
+<?php
+defined("BASEPATH") OR exit("No direct script access allowed");
+
+class REST_Controller extends CI_Controller
+{
+
+    protected $_supported_formats = [
+        'json' => 'application/json',
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();       
+        
+        // CORS
+        $this->httpcors->_authenticate_CORS();
+    }
+
+    protected function response($data, $status = 200)
+    {
+        //set response code
+        http_response_code($status); 
+        $this->to_json($data);
+        exit();
+    }
+
+    /**
+     * Retreive POST INPUT
+     */
+    protected function get_post($input)
+    {
+        $content_type = $this->input->server('CONTENT_TYPE');
+        $content_type = (strpos($content_type, ';') !== FALSE ? current(explode(';', $content_type)) : $content_type);
+        //cek jika input content type adalah JSON
+        if ($content_type == 'application/json') {
+            $body = json_decode($this->input->raw_input_stream, true);
+            return isset($body[$input]) ? $body[$input] : null;
+        } else {
+            return $this->input->post($input);
+        }
+    }
+
+    /**
+     * Retreive POST File
+     */
+    protected function get_file($input)
+    {
+        return isset($_FILES[$input]) ? $_FILES[$input] : null;
+    }
+
+    /**
+     * Set Output to JSON
+     */
+    protected function to_json($data)
+    {
+        return $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data, JSON_PRETTY_PRINT))->_display();
+    }
+
+}
