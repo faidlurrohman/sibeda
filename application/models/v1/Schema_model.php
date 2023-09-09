23d60bbca9bccddf988fc40801534ad45f80ac2d
Faidlur Rohman <faidrohman@outlook.com>
2023-09-09 18:21:37 +0700

    schema model

---
+++ application/models/v1/Schema_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Schema_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function get_field_type($table_name)
+    {
+        $sql = "
+            SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$table_name'
+        ";
+        $query = $this->db->query($sql);
+
+        return $query->result();
+    }
+
+}
