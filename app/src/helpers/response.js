faa6ca836614543dce50f6a3a67426493e69264d
faidlurrohman <faidrohman@outlook.com>
2024-04-06 22:18:31 +0700

    add delete action

--- app/src/helpers/response.js
+++ app/src/helpers/response.js
@@ -3,22 +3,26 @@ import { MESSAGE } from "./constants";
 import { swal } from "./swal";
 
 export const responseGet = (response) => {
-	let _fix = {
-		data: [],
-		total_count: 0,
-	};
+  let _fix = {
+    data: [],
+    total_count: 0,
+  };
 
-	if (!!response?.data?.data.length) {
-		_fix.total_count = response?.data?.data[0]?.__res_count || 0;
+  if (!!response?.data?.data.length) {
+    _fix.total_count = response?.data?.data[0]?.__res_count || 0;
 
-		_.map(response?.data?.data, (item) => {
-			if (_fix.total_count) _fix.data.push(JSON.parse(item?.__res_data));
-		});
-	}
+    _.map(response?.data?.data, (item) => {
+      if (_fix.total_count) _fix.data.push(JSON.parse(item?.__res_data));
+    });
+  }
 
-	return _fix;
+  return _fix;
 };
 
-export const messageAction = (isEdit = false) => {
-	swal(`${isEdit ? MESSAGE?.edit : MESSAGE?.add}.`, "success");
+export const messageAction = (isEdit = false, isDelete = false) => {
+  if (isDelete) {
+    swal(MESSAGE.delete, "success");
+  } else {
+    swal(`${isEdit ? MESSAGE?.edit : MESSAGE?.add}.`, "success");
+  }
 };
